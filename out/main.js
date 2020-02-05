dc511e0f557c1f482e1098d853ac2af8d54d6ad2
fmilke <fridjofmilke@googlemail.com>
2020-02-05 20:44:25 +0100

    added out dir to .gitignore

--- out/main.js
+++ out/main.js
@@ -1,3 +1,10 @@
 "use strict";
-console.log("Jo");
+Object.defineProperty(exports, "__esModule", { value: true });
+const Tokenizer_1 = require("./impl/Tokenizer");
+// var testString = "func Hit() { return Explode(50); }";
+var testString = "IdentOne IdentTwo Catch me if you can ";
+var tokenizer = new Tokenizer_1.Tokenizer(testString);
+for (let token of tokenizer) {
+    console.log(token.value);
+}
 //# sourceMappingURL=main.js.map
